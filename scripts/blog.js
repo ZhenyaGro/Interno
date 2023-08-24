8a4fbc6f37122eab4fc845a1eb28e220a2e9d670
ZhenyaGro <bon4ios@gmail.com>
2023-08-25 00:04:57 +0300

    Vue script data

---
+++ scripts/blog.js
@@ -0,0 +1,44 @@
+'use strict';
+new Vue({
+  el: '#app',
+  data: {
+    cards: [
+      {
+        imgPath: './images/article1.png',
+        sticker: 'Kitchen Design',
+        heading: 'Let’s Get Solution For Building Construction Work',
+        date: '26 December, 2022'
+      },
+      {
+        imgPath: './images/article2.png',
+        sticker: 'Living Design',
+        heading: 'Low Cost Latest Invented Interior Designing Ideas.',
+        date: '22 December, 2022 '
+      },
+      {
+        imgPath: './images/article3.png',
+        sticker: 'Interior Design',
+        heading: 'Best For Any Office & Business Interior Solution',
+        date: '25 December, 2022'
+      },
+      {
+        imgPath: './images/article4.png',
+        sticker: 'Kitchen Design',
+        heading: 'Let’s Get Solution For Building Construction Work',
+        date: '26 December, 2022'
+      },
+      {
+        imgPath: './images/article5.png',
+        sticker: 'Living Design',
+        heading: 'Low Cost Latest Invented Interior Designing Ideas.',
+        date: '22 December, 2022'
+      },
+      {
+        imgPath: './images/article6.png',
+        sticker: 'Interior Design',
+        heading: 'Best For Any Office & Business Interior Solution',
+        date: '25 December, 2022'
+      }
+    ]
+  }
+});
