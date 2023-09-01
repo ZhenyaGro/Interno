ac218629c8b075b83b98f9413898d26e47940f57
ZhenyaGro <bon4ios@gmail.com>
2023-09-02 00:14:02 +0300

    Component blog main in progress

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,18 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-  <BlogData></BlogData>
-  <BlogItem></BlogItem>
-  <BlogList></BlogList>
-  <TagList></TagList>
-  <BlogDetails></BlogDetails>
+  <BlogMain></BlogMain>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import BlogData from './components/BlogData.vue';
-import BlogDetails from './components/BlogDetails.vue';
-import BlogItem from './components/BlogItem.vue';
-import BlogList from './components/BlogList.vue';
-import TagList from './components/TagList.vue';
+import BlogMain from './components/BlogMain.vue';
 
 // const dataExchange = new Vue();
 export default {
   name: 'App',
   components: {
     // HelloWorld,
-    BlogData,
-    BlogDetails,
-    BlogItem,
-    BlogList,
-    TagList
+    BlogMain
   }
 }
 </script>
---
+++ src/components/BlogMain.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <BlogDetails></BlogDetails>
+    <TagList></TagList>
+  </div>
+</template>
+
+<script>
+import BlogDetails from './BlogDetails.vue';
+import TagList from './TagList.vue';
+export default {
+  components: {
+    BlogDetails,
+    TagList
+  }
+}
+</script>
