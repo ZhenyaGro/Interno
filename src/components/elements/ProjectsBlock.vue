e676ab2cb0a2ff15bee314e4e288fbee7684f32c
ZhenyaGro <bon4ios@gmail.com>
2023-09-06 23:01:29 +0300

    Projects Block component in progress

---
+++ src/components/elements/ProjectsBlock.vue
@@ -0,0 +1,28 @@
+<template>
+  <a href="#" class="projects__block">
+    <img :src="getImg" alt="project image" class="projects__img">
+    <!-- <div class="projects__img projects__img_1"></div> -->
+    <div class="projects__info">
+      <div class="projects__description">
+        <h3 class="projects__project-name">{{ heading }}</h3>
+        <p class="projects__category">{{ paragraph }}</p>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 70 70" fill="none">
+        <circle cx="35" cy="35" r="35" fill="#F4F0EC" />
+        <path d="M32 44L40 35L32 26" stroke="#292F36" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+      </svg>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'ProjectsBlock',
+  props: ['imgUrl', 'heading', 'paragraph'],
+  methods: {
+    getImg() {
+      return require(imgUrl);
+    }
+  },
+};
+</script>
