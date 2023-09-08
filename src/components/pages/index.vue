4f90d85a4ee29432dd0a2951a0d8601e079496ec
ZhenyaGro <bon4ios@gmail.com>
2023-09-08 23:48:42 +0300

    Page index

--- src/components/pages/index.vue
+++ src/components/pages/index.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
+    <Header />
+    <IndexMain />
+    <OurProjects />
+    <Results />
+    <News />
+    <Footer />
   </div>
 </template>
 
 <script>
 import styles from '../styles/style.css'
+import Header from './components/elements/Header.vue';
+import IndexMain from './components/elements/IndexMain.vue';
+import OurProjects from './components/elements/OurProjects.vue';
+import Results from './components/elements/Results.vue';
+import News from './components/elements/News.vue';
+import Footer from './components/elements/Footer.vue';
 
-// const dataExchange = new Vue();
+
 export default {
-  name: 'index',
+  name: 'App',
   components: {
+    Header,
+    IndexMain,
+    OurProjects,
+    Results,
+    News,
+    Footer
   }
 }
 </script>
