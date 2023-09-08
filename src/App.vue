dfe44c6b506efb6c62385b665a9284798605253d
ZhenyaGro <bon4ios@gmail.com>
2023-09-08 23:12:14 +0300

    Component Results

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    <Header></Header>
-    <IndexMain></IndexMain>
-    <OurProjects></OurProjects>
-    <Footer></Footer>
+    <Header />
+    <IndexMain />
+    <OurProjects />
+    <Results />
+    <Footer />
   </div>
 </template>
 
@@ -12,7 +13,9 @@ import styles from '../styles/style.css'
 import Header from './components/elements/Header.vue';
 import IndexMain from './components/elements/IndexMain.vue';
 import OurProjects from './components/elements/OurProjects.vue';
+import Results from './components/elements/Results.vue';
 import Footer from './components/elements/Footer.vue';
+
 
 export default {
   name: 'App',
@@ -20,6 +23,7 @@ export default {
     Header,
     IndexMain,
     OurProjects,
+    Results,
     Footer
   }
 }
