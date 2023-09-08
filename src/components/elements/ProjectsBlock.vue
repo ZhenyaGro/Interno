0eb4f52130f0f45bdaa2209494c7e11de6af1b24
ZhenyaGro <bon4ios@gmail.com>
2023-09-08 23:10:52 +0300

    Add styles

--- src/components/elements/ProjectsBlock.vue
+++ src/components/elements/ProjectsBlock.vue
@@ -20,3 +20,87 @@ export default {
   props: ['imgUrl', 'heading', 'paragraph'],
 };
 </script>
+
+<style lang="scss">
+@import '/styles/_variables.scss';
+
+.projects {
+  margin-bottom: 96px;
+
+  &__header {
+    text-align: center;
+  }
+
+  &__heading {
+    font-size: 50px;
+    line-height: 125%; // 62.5px
+    letter-spacing: 1px;
+  }
+
+  &__text {
+
+    @extend %textAdditional;
+    letter-spacing: 0.22px;
+    margin: 0 auto;
+    max-width: 737px;
+    margin-bottom: 93px;
+  }
+
+  &__content {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(547px, 1fr));
+    column-gap: 100px;
+    row-gap: 56px;
+  }
+
+  &__block {
+    width: 547px;
+    display: block;
+  }
+
+  &__block:first-child>img {
+    border-radius: 0 80px 0 0;
+  }
+
+  &__block:nth-child(2)>img {
+    border-radius: 80px 0 0 0;
+  }
+
+  &__block:nth-child(3)>img {
+    border-radius: 0 0 80px 0;
+  }
+
+  &__block:last-child>img {
+    border-radius: 0 0 0 80px;
+  }
+
+  &__block svg {
+
+    path,
+    circle {
+      transition: all .2s;
+    }
+  }
+
+  &__img {
+    margin-bottom: 24px;
+  }
+
+  &__info {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  &__project-name {
+    font-size: 25px;
+    line-height: 125%; // 31.25px
+    letter-spacing: 0.5px;
+  }
+
+  &__category {
+    @extend %textAdditional;
+    letter-spacing: 0.22px;
+  }
+
+}
+</style>
