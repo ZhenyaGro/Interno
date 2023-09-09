238802683046192bfd4d8e18f0cc573b97b4d733
ZhenyaGro <bon4ios@gmail.com>
2023-09-09 16:18:27 +0300

    Blog storage

---
+++ src/components/store/blog.js
@@ -0,0 +1,15 @@
+export default {
+  state() {
+    return {
+      pageView: {
+        heading: 'Articles & News',
+        breadcrumbs: ['Home', 'Blog']
+      }
+    }
+  },
+  getters: {
+    getBlogPageViewData(state) {
+      return state.pageView;
+    }
+  }
+}
--- src/components/store/index.js
+++ src/components/store/index.js
@@ -2,11 +2,13 @@ import { createStore } from 'vuex';
 
 import storeUniversal from './universal.js';
 import storeIndex from './indexPage.js';
+import storeBlog from './blog.js'
 
 export default createStore({
   modules: {
     storeUniversal,
     storeIndex,
+    storeBlog,
   }
 });
 
