9d99a9e4131caa06a9210d36ae03797413a4cbc5
ZhenyaGro <bon4ios@gmail.com>
2023-09-16 20:30:30 +0300

    Create store

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import { createStore } from 'vuex';
 import storeUniversal from './universal.js';
 import storeIndex from './home.js';
 import storeBlog from './blog.js'
+import blogDetails from './blogdetails.js';
 
 export default createStore({
   modules: {
     storeUniversal,
     storeIndex,
     storeBlog,
+    blogDetails
   }
 });
 
