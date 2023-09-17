5e0ded04c817038413f08744818c0c95e487552e
ZhenyaGro <bon4ios@gmail.com>
2023-09-17 20:58:31 +0300

    Add project page view to component

--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
-
+    <PageView class="page-view" />
+    <p>test</p>
   </div>
 </template>
 
 <script>
+import PageView from '@/components/PageView.vue';
+
 export default {
   name: 'Project',
-
+  components: {
+    PageView
+  }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '/styles/_variables.scss';
+
+.page-view {
+  @include page-view_background('/public/img/project-view.jpg', 356);
+}
+</style>
